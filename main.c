0b7bafe189ffcce605ada6609790e51375f633fa
Francocarabajal78 <62974003+Francocarabajal78@users.noreply.github.com>
2020-04-29 14:31:39 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+void circulo();
+void cruz();
+void rayahori();
+void rayaverti();
+int numele();
+int filas();
+int columnas1();
+int columnas2();
+int máquina();
 int main()
 {
     int ele_usu=0; //elección del usuario de intervalo (1,9)
     int posicion_usuario; // posiciín en el tablero correspondiente a la eleccion del usuario (0,8)
+    int posicion_máquina; //posicion en el tablero correspondiente a la que elige la máquina (0,8)
     gotoxy(45,12);
     rayahori();
     gotoxy(45,14);
@@ -16,6 +26,9 @@ int main()
     printf("elija un numero entre 1 y 9\n");
     scanf("%d", & ele_usu);
     posicion_usuario=numele(ele_usu);
+    cruz();
+    posicion_máquina=máquina();
+    circulo();
     return 0;
 }
 void rayahori() //Función para crear líneas horizontales del tablero
@@ -33,13 +46,27 @@ void rayaverti() //Funcion para crear líneas verticales del tablero
         printf("%c", 179);
     }
 }
+void circulo() //Esta función crea los circulos manejados por la maquina
+{
+    printf("%c", 79);
+}
+void cruz() //Esta funcion crea las cruces controladas por el usuario
+{
+    printf("%c", 88);
+}
+int máquina(ele_maq) //Función que representa la jugada de la máquina
+{
+    srand(time(NULL))
+    ele_maq=rand()%(8-0+0)+0; //el valor aleatorio que va a tomar la elección de la máquina
+    return ele_maq;
+}
 int numele(int num_ele) //Esta funcion devuelve un valor entre el intervalo (0,8)
 {
   return num_ele--;
 }
 int filas(int fila) //Esta funcion devuelve el valor de filas del intervalo (0,8)
 {
-    fila=numele()/3;
+    fila=numele(fila)/3;
     return fila;
 }
 int columnas1(int columna1) //Esta funcion devuelve el valor de columnas 4 5 y 6 del intervalo (0,8)
