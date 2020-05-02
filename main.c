480d4f4761e699ad30935a3442ae1db51fb511bd
Francocarabajal78 <62974003+Francocarabajal78@users.noreply.github.com>
2020-05-01 22:56:17 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,69 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-void circulo();
-void cruz();
-void rayahori();
-void rayaverti();
+void dibujarcirculo();
+void dibujarcruz();
+void inicializartablero();
+void mostrartablero();
 int numele();
 int filas();
 int columnas1();
 int columnas2();
-int máquina();
+int inicializarmáquina();
 int main()
 {
     int ele_usu=0; //elección del usuario de intervalo (1,9)
     int posicion_usuario; // posiciín en el tablero correspondiente a la eleccion del usuario (0,8)
     int posicion_máquina; //posicion en el tablero correspondiente a la que elige la máquina (0,8)
-    gotoxy(45,12);
-    rayahori();
-    gotoxy(45,14);
-    rayahori();
-    gotoxy(3,10);
-    rayaverti();
-    gotoxy(3,1);
-    rayaverti();
+    char tablero[9];
+    inicializartablero(tablero);
+    mostrartablero(tablero);
     printf("elija un numero entre 1 y 9\n");
     scanf("%d", & ele_usu);
-    posicion_usuario=numele(ele_usu);
-    cruz();
-    posicion_máquina=máquina();
+    posicion_usuario=numeroelegido(ele_usu);
+    dibujarcruz();
+    posicion_máquina=inicializarmáquina();
     circulo();
     return 0;
 }
-void rayahori() //Función para crear líneas horizontales del tablero
+void inicializartablero(char tablero[]) //crea al tablero para darle un valor a futuro
 {
-    for(int i=0; i<15; i++)
+    for(int x=0; x<9; x++)
     {
-        printf("%c", 196);
+        tablero[x]=''; //lo que se hace aca es dejar todos los elementos del vector "tablero" en blanco o vacios
     }
 }
-void rayaverti() //Funcion para crear líneas verticales del tablero
+void mostrartablero()
 {
-    for(int i=0; i<8; i++)
-    {
-        printf("\n");
-        printf("%c", 179);
-    }
+    printf("%c| %c| %c\n", tablero[0], tablero[1], tablero[2]);
+    printf("---|---|---\n");
+    printf("%c| %c| %c\n", tablero[3], tablero[4], tablero[5]);
+    printf("---|---|---\n");
+    printf("%c| %c| %c\n", tablero[6], tablero[7], tablero[8]);
 }
-void circulo() //Esta función crea los circulos manejados por la maquina
+void dibujarcirculo() //Esta función crea los circulos manejados por la maquina
 {
     printf("%c", 79);
 }
-void cruz() //Esta funcion crea las cruces controladas por el usuario
+void dibujarcruz() //Esta funcion crea las cruces controladas por el usuario
 {
     printf("%c", 88);
 }
-int máquina(ele_maq) //Función que representa la jugada de la máquina
+int inicializarmáquina(ele_maq) //Función que representa la jugada de la máquina
 {
     srand(time(NULL))
     ele_maq=rand()%(8-0+0)+0; //el valor aleatorio que va a tomar la elección de la máquina
     return ele_maq;
 }
-int numele(int num_ele) //Esta funcion devuelve un valor entre el intervalo (0,8)
+int numeroelegido(int num_ele) //Esta funcion devuelve un valor entre el intervalo (0,8)
 {
   return num_ele--;
 }
+/*
 int filas(int fila) //Esta funcion devuelve el valor de filas del intervalo (0,8)
 {
     fila=numele(fila)/3;
